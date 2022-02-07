4b8bc1c55abc432be4f5b5f9692388903fb2b3e8
ishita <ishitas654@gmail.com>
2022-02-08 00:58:42 +0530

    session-2

---
+++ Event Handling/app.js
@@ -0,0 +1,34 @@
+
+
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name:'',
+      confirmedName: ''
+    };
+  },
+  methods:{
+    confirmInput() {
+this.confirmedName = this.name;
+    },
+    submitForm(){
+     
+      alert('Submitted!');
+    },
+    setName(event, lastName){
+      this.name = event.target.value + ' ' + lastName ;
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    Reduce(num){
+      this.counter = this.counter - num;
+    },
+
+  
+  }
+
+});
+
+app.mount('#events');
